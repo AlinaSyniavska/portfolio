d3ebfd01a16ff013c9fa049124aa59bc07a9e012
AlinaSyniavska <alina22syniavska@gmail.com>
2023-01-24 00:57:44 +0200

    add fontawesome's icons

--- src/components/ProfileContact/ProfileContact.tsx
+++ src/components/ProfileContact/ProfileContact.tsx
@@ -1,4 +1,15 @@
 import {FC} from "react";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {
+    faPhone,
+    faLocationDot,
+    faSquareEnvelope,
+} from "@fortawesome/free-solid-svg-icons";
+import {
+    faGithub,
+    faLinkedin,
+} from "@fortawesome/free-brands-svg-icons";
+
 import {IPerson} from "../../interfaces";
 
 interface IProps {
@@ -8,7 +19,11 @@ interface IProps {
 const ProfileContact: FC<IProps> = ({contacts}) => {
     return (
         <div>
-            ProfileContact
+            <FontAwesomeIcon icon={faPhone} title={'Edit'}/>
+            <FontAwesomeIcon icon={faLocationDot} title={'Edit'}/>
+            <FontAwesomeIcon icon={faSquareEnvelope} title={'Edit'}/>
+            <FontAwesomeIcon icon={faGithub} title={'Edit'}/>
+            <FontAwesomeIcon icon={faLinkedin} title={'Edit'}/>
         </div>
     );
 };
---
+++ src/fontawesome.ts
@@ -0,0 +1,15 @@
+// import the library
+import { library } from '@fortawesome/fontawesome-svg-core';
+
+// import your icons
+import {faPhone, faLocationDot, faSquareEnvelope, faChevronRight} from "@fortawesome/free-solid-svg-icons";
+import {faGithub, faLinkedin} from "@fortawesome/free-brands-svg-icons";
+
+library.add(
+    faPhone,
+    faLocationDot,
+    faSquareEnvelope,
+    faGithub,
+    faLinkedin,
+    faChevronRight
+);
