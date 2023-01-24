e64e7edea9a813bc6732ad872f840f3fa5d68075
AlinaSyniavska <alina22syniavska@gmail.com>
2023-01-25 00:13:45 +0200

    fix project interface

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,5 +1,6 @@
 export * from './education.interface';
 export * from './language.interface';
 export * from './person.interface';
+export * from './project.interface';
 export * from './social.interface';
 
---
+++ src/interfaces/project.interface.ts
@@ -0,0 +1,13 @@
+export interface IProject {
+    title: string,
+    description: string,
+    stack: string[],
+    links: {
+        front?: string,
+        back?: string,
+        image?: string,
+        website?: string,
+        video?: string,
+    },
+}
+
