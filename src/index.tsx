93496fa21c21db0e099e13f7382286de658da130
AlinaSyniavska <alina22syniavska@gmail.com>
2023-01-29 14:23:55 +0200

    fix createBrowserHistory

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {BrowserRouter} from "react-router-dom";
+
+import {unstable_HistoryRouter as BrowserRouter} from "react-router-dom";
+// import {createBrowserHistory} from "history";
+import {createBrowserHistory} from "@remix-run/router";
 
 import {App} from './App';
 import './fontawesome';
 
+// const history = createBrowserHistory({ window });
+const history = createBrowserHistory({ v5Compat: true });
+
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
+
 root.render(
     <React.StrictMode>
-        <BrowserRouter>
+        <BrowserRouter history={history}>
             <App/>
         </BrowserRouter>
     </React.StrictMode>
